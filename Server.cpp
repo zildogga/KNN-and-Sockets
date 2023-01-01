8e6211e183b84692950750f0366cd4c868df347a
zildogga <114800330+zildogga@users.noreply.github.com>
2023-01-01 17:09:35 +0200

    almost finished server class (waiting for client class), and made a new function in Classification.cpp to get a vector and return a class string instead of getting a vector inside the function

--- Server.cpp
+++ Server.cpp
@@ -6,6 +6,9 @@
 
 string file = "";
 int sockNum = 0;
+int size = 4096;
+// This line creates a new Classification object
+Classification classification;
 
 int Server::startServer(int serverPort, string fileName) {
     file = fileName;
@@ -33,6 +36,8 @@ int Server::startServer(int serverPort, string fileName) {
         return -1;
     }
     sockNum = sock;
+    while(acceptClient()) {}
+    close(sock);
     return 0;
 }
 int Server::acceptClient() {
@@ -43,23 +48,82 @@ int Server::acceptClient() {
         perror("error accepting client");
         return -1;
     }
-    return 0;
+    int check = 1;
+    while(check) {
+        string result = serverClassify(client_sock);
+        if(result == "") {
+            check = 0;
+        }
+        if(result == "close") {
+            return 0;
+        }
+    }
 }
-int classify() {
-    char buffer[4096];
+string Server::serverClassify(int clientSock) {
+    string result = "";
+    char buffer[size];
     int expected_data_len = sizeof(buffer);
-    int read_bytes = recv(client_sock, buffer, expected_data_len, 0);
+    int read_bytes = recv(clientSock, buffer, expected_data_len, 0);
     if (read_bytes == 0) {
-// connection is closed
+        //connection is closed
+        return "";
     }
     else if (read_bytes < 0) {
-// error
+        // error
+        return "";
     }
     else {
-        cout << buffer;
+        //here should be the classification
+
+        //get out of the buffer the values with integrity checks
+        vector<double> vec;
+        int k;
+        string disType="";
+
+        istringstream iss(buffer); // create an input string stream from the input string
+
+        double num; // variable to store the current integer
+        string str; // variable to store the string
+        int count = 0; // variable to store the number of integers extracted so far
+        //closing the server if the buffer have -1 and that's it
+        if(buffer[0] == -1 && buffer[1] == '\0') {
+            return "close";
+        }
+        // try to extract values from the input string until the string stream fails
+        while (iss >> num) {
+            ++count; // increment the count of extracted integers
+        }
+
+        // check if the number of integers is at least 2 and the string was extracted successfully
+        if (count >= 1 && iss >> str >> num) {
+            // the input string has the correct format, so extract the values again
+            iss.clear(); // clear the string stream's fail flag
+            iss.seekg(0); // reset the string stream's position to the beginning
+
+            // extract the integers and string
+            for (int i = 0; i < count - 1; ++i) {
+                iss >> num;
+                vec.push_back(num);
+            }
+            iss >> str >> num;
+            disType = str;
+            k = num;
+            // This line calls the inputToClass function to classify the test case
+            result = classification.vectorToClass(file, k, disType, vec);
+            result.copy(buffer, size-1);
+            //might need +1 or -1
+            buffer[result.size()] = '\0';
+        } else {
+            // the input string does not have the correct format
+            result = "invalid input";
+            result.copy(buffer, size-1);
+            //might need +1 or -1
+            buffer[result.size()] = '\0';
+        }
     }
-    int sent_bytes = send(client_sock, buffer, read_bytes, 0);
+    int sent_bytes = send(clientSock, buffer, read_bytes, 0);
     if (sent_bytes < 0) {
         perror("error sending to client");
     }
+    return "good";
 }
