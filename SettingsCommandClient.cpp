dd45361101ca2226696a788e06934e9a886844a2
zildogga <114800330+zildogga@users.noreply.github.com>
2023-01-21 23:36:15 +0200

    fixed client SettingsCommandClient.cpp

--- SettingsCommandClient.cpp
+++ SettingsCommandClient.cpp
@@ -6,18 +6,33 @@
 
 void SettingsCommandClient::execute() {
     SocketIO scio(sock);
+    cout << "1" << endl;
     dio = &scio;
+    cout << "2" << endl;
     dio->write("2");
+    cout << "3" << endl;
     char* serverStr = dio->read();
+    cout << "4" << endl;
     StandardIO sdio;
+    cout << "5" << endl;
     dio = &sdio;
+    cout << "6" << endl;
     string str1(serverStr);
+    cout << "7" << endl;
     dio->write(str1);
+    cout << "8" << endl;
     char* userString = dio->read();
+    cout << "9" << endl;
     dio = &scio;
+    cout << "10" << endl;
     dio->write(userString);
+    cout << "11" << endl;
     serverStr = dio->read();
+    cout << "12" << endl;
     dio = &sdio;
+    cout << "13" << endl;
     string str2(serverStr);
+    cout << "14" << endl;
     dio->write(str2);
+    cout << "15" << endl;
 }
