f9e247c308aa50ed6d9088c73b99e8468ed1f56e
zildogga <114800330+zildogga@users.noreply.github.com>
2023-01-23 22:20:42 +0200

    testing 5

--- DownloadCommandClient.cpp
+++ DownloadCommandClient.cpp
@@ -3,10 +3,14 @@ void DownloadCommandClient::execute() {
     string serverStr;
     dio = &scio;
     vector<string> vs;
+    cout << "before while" << endl;
     while ((serverStr = scio.readFive()) != "Done.") {
-        dio->write("ok");
+        cout << "before ok" << endl;
+        scio.write("ok");
+        cout << "after ok" << endl;
         vs.push_back(serverStr);
     }
+    cout << "after while" << endl;
     ReadFile rf;
     rf.WriteCSVByVector(vs,path,"Classified.csv");
     return;
